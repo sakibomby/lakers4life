4a0f4c3102e2446567a462089256fa900763b56d
sakibomby <csakae@gmail.com>
2024-04-24 09:49:49 -0700

    Edit Update and Delete Comments

--- controllers/comments.js
+++ controllers/comments.js
@@ -2,7 +2,39 @@ const Post = require('../models/post');
 
 module.exports = {
     create,
+    edit,
+    update,
+    delete: deleteComment
 };
+
+async function deleteComment(req, res) {
+    const post = await Post.findOne({'comments_id': req.params.id, 'user': req.user._id});
+    if (!post) return res.redirect(`/posts/${req.params.id}`);
+    post.comments.remove(req.params.id);
+    await post.save();
+    res.redirect(`/posts/${req.params.id}`);
+}
+
+async function update(req, res) {
+    const post = await Post.findOne({ 'comments._id': req.params.id });
+    const comment = post.comments.id(req.params.id);
+    if (!comment.user.equals(req.user._id)) return res.redirect(`/posts/${req.params.id}`);
+    comment.comment = req.body.comment;
+    console.log(comment.comment);
+    try {
+        await post.save();
+    } catch (err) {
+        console.log(err.message);
+    }
+    res.redirect(`/posts/${req.params.id}`);
+};
+
+async function edit(req, res) {
+    const post = await Post.findOne({'comments._id': req.params.id});
+    const comment = post.comments.id(req.params.id);
+    res.render('comments/edit', {post, comment});
+  }
+
 
 async function create(req, res) {
     try {
--- routes/comments.js
+++ routes/comments.js
@@ -3,7 +3,14 @@ const router = express.Router();
 const commentsCtrl = require('../controllers/comments');
 const ensureLoggedIn = require('../config/ensureLoggedIn');
 
+
+// GET /comments/:id/edit
+router.get('/comments/:id/edit', ensureLoggedIn, commentsCtrl.edit);
 // POST /posts/:id/comments
 router.post('/posts/:id/comments', ensureLoggedIn, commentsCtrl.create);
+// PUT /comments/:id
+router.put('/comments/:id', ensureLoggedIn, commentsCtrl.update); 
+// DELETE /comments/:id
+router.delete('comments/:id', ensureLoggedIn, commentsCtrl.delete);
 
 module.exports = router;
